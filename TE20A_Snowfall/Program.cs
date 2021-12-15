06bb8392594327fa0697bc06fbfd6950bc86475e
Mikael Bergström <krank23@gmail.com>
2021-12-15 13:32:59 +0100

    Initial commit

---
+++ TE20A_Snowfall/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Snowfall");
+Raylib.SetTargetFPS(60);
+
+Random generator = new Random();
+List<Rectangle> snowflakes = new List<Rectangle>();
+List<float> snowSpeed = new List<float>();
+
+for (int i = 0; i < 2000; i++)
+{
+  int x = generator.Next(Raylib.GetScreenWidth());
+  int y = generator.Next(Raylib.GetScreenHeight());
+
+  int size = generator.Next(2, 7);
+  float speed = (float) (generator.NextDouble() + 0.5);
+  snowSpeed.Add(speed);
+  snowflakes.Add(new Rectangle(x, y, size, size));
+}
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  Raylib.ClearBackground(Color.SKYBLUE);
+
+  for (int i = 0; i < snowflakes.Count; i++)
+  {
+    Rectangle flake = snowflakes[i];
+    flake.y += snowSpeed[i];
+    if (flake.y > 600)
+    {
+      flake.y = -10;
+    }
+    snowflakes[i] = flake;
+
+    Raylib.DrawRectangleRec(snowflakes[i], Color.WHITE);
+  }
+
+  Raylib.EndDrawing();
+}
